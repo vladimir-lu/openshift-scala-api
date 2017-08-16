dd91d028ccb115df388c5d46c9aeee27270d0f79
Vladimir Lushnikov <vladimir@solidninja.is>
2017-08-16 09:10:40 +0100

    Release 0.0.3

--- project/build.scala
+++ project/build.scala
@@ -44,7 +44,7 @@ object build {
 
   val commonSettings = mavenCentralSettings ++ Seq(
     organization := "is.solidninja.openshift",
-    version := "0.0.2",
+    version := "0.0.3",
     scalaVersion := "2.12.3",
     crossScalaVersions := Seq("2.11.11", "2.12.3"),
     scalacOptions ++= Seq(
